13121851249253ae1e70ebb33a4b80a9e71f38d6
Steven <stloperag@gmail.com>
2024-05-13 22:49:15 -0500

    Encapsulamiento - Implementación de Clase Persona

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+
+    public class Persona {
+        // Propiedades privadas
+        private String nombre;
+        private int edad;
+        private LocalDate fechaNacimiento;
+    }
+
+}
+
